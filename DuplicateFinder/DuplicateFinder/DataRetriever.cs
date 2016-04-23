23b7d59f9570a43b1a9d46ea36d45ec2bfb1e219
Matthew <matthew.s.pierce@uconn.edu>
2016-04-23 02:24:20 -0400

    Adding some simple class stubs

---
+++ DuplicateFinder/DuplicateFinder/DataRetriever.cs
@@ -0,0 +1,16 @@
+﻿/* Class: DataRetriever
+ * This class serves as a simple Data Access interface between Excel and the program.
+ * All data requests and manipulations should come through this class.
+ * */
+
+
+using System;
+using System.Collections.Generic;
+using GemBox.Spreadsheet;
+
+namespace DuplicateFinder
+{
+    class DataRetriever
+    {
+    }
+}
---
+++ DuplicateFinder/DuplicateFinder/Record.cs
@@ -0,0 +1,25 @@
+﻿/* Class: Record
+ * This class is a simple DAO for the rows pulled back from the Excel spreadsheet.
+ * Note that data that will not be used for duplicate detection is ommitted
+ * */
+
+using System;
+
+namespace DuplicateFinder
+{
+    class Record
+    {
+        private String firstName, lastName, middleName;
+        private DateTime claimDate;
+        private long claimNumber;
+
+        public Record(String first, String last, String middle, DateTime date, long claimNum)
+        {
+            firstName = first;
+            lastName = last;
+            middleName = middle;
+            claimDate = date;
+            claimNumber = claimNum;
+        }
+    }
+}
